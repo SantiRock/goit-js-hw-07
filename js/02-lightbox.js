c8f8bf7432282a81ee4c1f780de94cd2da7b55b7
Santiago <santiago@192.168.0.5>
2023-01-11 13:12:07 -0500

    parte 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+for (let item of galleryItems) {
+
+    const link = document.createElement("a");
+    link.classList.add("gallery__item");
+    link.href = item.original;
+    gallery.append(link);
+
+    const image = document.createElement("img");
+    image.classList.add("gallery__image");
+    image.src = item.preview;
+    image.alt = item.description;
+    link.append(image);
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+    });
+};
+
+
+let lightbox = new SimpleLightbox('.gallery a', { gallery });
+lightbox.options.captionsData = "alt";
+lightbox.options.captionDelay = 250;
+
+console.log(lightbox);
+console.log(gallery);
+
